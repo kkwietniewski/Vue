c86d57dc47336b50851daf45a5c8d8bbb2735abc
Kacper Kwietniewski <48916089+kkwietniewski@users.noreply.github.com>
2021-05-27 20:23:25 +0200

    Random user generator

---
+++ random_user_gen/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            firstName: 'Marek',
+            lastName: 'Nowak',
+            email: 'Nowczi@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/78.jpg',
+        }
+    },
+    methods:{
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api/')
+            const { results } = await res.json()
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        }
+    }
+})
+
+
+app.mount("#app")
